68eb981c7c7b8b06e765107db7bb5c21de086c4a
shemer binner <shemer146@gmail.com>
2022-05-02 17:29:38 +0300

    logo login changes

--- src/views/login-app.vue
+++ src/views/login-app.vue
@@ -1,9 +1,7 @@
 <template>
   <section class="login-cmp">
     <div class="logo-container">
-      <div class="logo-img">
-        <!-- <img src="../../src/assets/icons/trello-blue.svg" /> -->
-      </div>
+      <img class="logo-img" src="../../src/assets/icons/trello-blue.svg" />
       <h1>Duello</h1>
     </div>
     <div class="login-container">
